fb773e7993c1616979c682fc772ce79fd0c63b83
kamirov <andrei.khramtsov@gmail.com>
2022-03-29 01:30:58 -0400

    Add ordering provider

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,15 @@ import {Provider} from "react-redux";
 import {CssBaseline} from "@material-ui/core";
 import {BrowserRouter} from "react-router-dom";
 import store from "./redux/store";
+import {HTML5Backend} from "react-dnd-html5-backend";
+import {DndProvider} from "react-dnd";
 ReactDOM.render(
     <Provider store={store}>
         <CssBaseline/>
         <BrowserRouter>
-            <App/>
+            <DndProvider backend={HTML5Backend}>
+                <App/>
+            </DndProvider>
         </BrowserRouter>
     </Provider>,
     document.getElementById('root')
