7a7bc49c410a96b0aea85d4f8efc856c65d93887
kamirov <andrei.khramtsov@gmail.com>
2022-03-29 01:28:40 -0400

    Add ordering

--- src/modules/ordering/OrderableList.tsx
+++ src/modules/ordering/OrderableList.tsx
@@ -1,7 +1,14 @@
 import * as React from "react";
+import OrderableItem from "./OrderableItem";
+import {useCallback} from "react";
+import {Orderable} from "./ordering.types";
+import update from 'immutability-helper'
 
 type Props = {
     label?: string
+
+    items: Orderable[]
+    onReorder: (items: Orderable[]) => void
 
     id?: string
     className?: string
@@ -12,10 +19,36 @@ function OrderableList(props: Props) {
 
     const label = props.label ? <span className="label">{props.label}</span> : null
 
+    const moveItem = useCallback((startIndex: number, endIndex: number) => {
+        const reorderedItems = update(props.items, {
+            $splice: [
+                [startIndex, 1],
+                [endIndex, 0, props.items[startIndex] as Orderable],
+            ],
+        })
+        props.onReorder(reorderedItems)
+    }, [props.items])
+
+    const renderItem = useCallback(
+        (item: Orderable, index: number) => {
+            return (
+                <OrderableItem
+                    key={item.id}
+                    index={index}
+                    id={item.id}
+                    moveItem={moveItem}
+                >
+                    {item.el}
+                </OrderableItem>
+            )
+        },
+        [props.items],
+    )
+
     return <div id={props.id} className={`${props.className} orderable-list-container`}>
         {label}
         <div className="orderable-list">
-            {props.children}
+            {props.items.map((item, i) => renderItem(item, i))}
         </div>
     </div>
 }
