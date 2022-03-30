bee82940d44cee5b3214870b49e5f9c09bd01852
kamirov <andrei.khramtsov@gmail.com>
2022-03-29 20:54:43 -0400

    Add a priority selection strategy

--- src/modules/AppView.tsx
+++ src/modules/AppView.tsx
@@ -237,7 +237,7 @@ export default function AppView() {
         if (learningMoves.length) {
             strategicArrays.push({
                 arr: learningMoves,
-                strategy: 'random'
+                strategy: 'priority'
             })
         }
 
--- src/modules/selection/SelectionService.ts
+++ src/modules/selection/SelectionService.ts
@@ -4,7 +4,7 @@ export const SelectionService = {
     select
 }
 
-export type SelectionStrategy = 'random'
+export type SelectionStrategy = 'random' | 'priority'
 
 export type StrategicArray = {
     arr: any[],
@@ -14,7 +14,17 @@ export type StrategicArray = {
 function select(strategicArrays: StrategicArray[]) {
     const strategicArray = ArrayHelper.randomElement(strategicArrays)
 
-    if (strategicArray.strategy === 'random') {
-        return ArrayHelper.randomElement(strategicArray.arr)
+    switch (strategicArray.strategy) {
+        case 'random':
+            return ArrayHelper.randomElement(strategicArray.arr)
+        case 'priority':
+            const priorityItems = 3     // If changing this, make sure to change it in CSS as well for .orderable-item
+            const priorityArr = strategicArray.arr.slice(0, priorityItems)
+            const nonPriorityArr = strategicArray.arr.slice(priorityItems)
+
+            console.log(priorityArr, nonPriorityArr)
+
+            const subArrayToPickFrom = ArrayHelper.randomElement([priorityArr, nonPriorityArr])
+            return ArrayHelper.randomElement(subArrayToPickFrom)
     }
 }
