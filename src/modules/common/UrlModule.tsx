2eab42162bc4abb6470f96752b425399e3cb5847
kamirov <andrei.khramtsov@gmail.com>
2022-03-29 22:29:47 -0400

    Remove extra escape

--- src/modules/common/UrlModule.tsx
+++ src/modules/common/UrlModule.tsx
@@ -4,7 +4,7 @@ export const UrlModule = {
 
 // Prepends protocol if needed; swiped from https://stackoverflow.com/a/24657561/745434
 function qualify(possibleUrl: string) {
-    if (!/^(?:f|ht)tps?\:\/\//.test(possibleUrl)) {
+    if (!/^(?:f|ht)tps?:\/\//.test(possibleUrl)) {
         possibleUrl = "https://" + possibleUrl;
     }
     return possibleUrl;
