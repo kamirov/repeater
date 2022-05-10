960aa2d19ed92f9ae8f3cee6511d61b9ab93925f
kamirov <andrei.khramtsov@gmail.com>
2022-05-10 18:42:40 -0400

    Add initial files

---
+++ infra/app-web/_main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "repeater-infra"
+    key            = "terraform.tfstate"
+    encrypt        = true
+    region         = "us-east-1"
+    dynamodb_table = "repeater-state-locks"
+  }
+
+  required_version = ">= 1.1.5"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
