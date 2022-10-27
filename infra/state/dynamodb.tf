48c94dbe58b08c28c32ecfdfcaa9a6c08fa98034
kamirov <andrei.khramtsov@gmail.com>
2022-10-26 21:52:59 -0400

    mika-1 migration

--- infra/state/dynamodb.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_dynamodb_table" "tf_state_locks" {
-  hash_key = "LockID"
-  name     = "repeater-state-locks"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-  billing_mode = "PAY_PER_REQUEST"
-}
---
+++ service-state/infra/_locals.tf
@@ -0,0 +1,15 @@
+locals {
+  org = "mika-1"
+  env = "prod"
+  app = "repeater"
+  component = "app-web"
+  namespace = "${local.org}-${local.app}"
+  fqn = "${local.namespace}-${local.env}-${local.component}"
+
+  default_tags = {
+    App = local.app
+    Component = local.component
+    Environment = local.env
+    Organization = local.org
+  }
+}
